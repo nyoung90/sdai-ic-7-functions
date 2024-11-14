17ee6ffee59f6f22487c8922c9d4e785213fd7af
nyoung90 <musicislovellc@gmail.com>
2024-11-14 05:31:15 +0000

    added function for eligibility for free shipping

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 // Type node script.js in the codespaces console to run this file. Make sure to save before running it. CTRL + ` will open the console if you don't see it.
+function isEligibleForFreeShipping(purchaseAmount, isMember) {
+  let freeShipping; 
+  if (isMember === true) {
+    freeShipping = true 
+  } else if (purchaseAmount >= 50) {
+    freeShipping = true 
+  } else {
+    freeShipping = false
+  }
+  console.log ("freeShipping", freeShipping)
+}
+
+// Test cases
+isEligibleForFreeShipping(30, true);  // You are eligible for free shipping.
+isEligibleForFreeShipping(60, false); // You are eligible for free shipping.
+isEligibleForFreeShipping(40, false); // You are not eligible for free shipping.
+isEligibleForFreeShipping(10, true);  // You are eligible for free shipping.
